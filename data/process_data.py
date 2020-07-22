442cbc80598ab4f0fa3bbabed064756923e70831
mpbnka <acharyamudumbi428@gmail.com>
2020-07-22 15:19:11 +0530

    Adding code to ML and ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,60 @@
+# import libraries
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 import sys
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Load data from the csv files
 
+    Args:
+        messages_filepath (String): Path to messages csv
+        categories_filepath (String): Path to categories csv
+
+    Returns:
+        df(pandas.DataFrame): pandas dataframe object
+    """
+    # load messages dataset
+    messages = pd.read_csv('disaster_messages.csv')
+    # load categories dataset
+    categories = pd.read_csv('disaster_categories.csv')
+    # merge datasets
+    df = messages.merge(categories, on=["id"])
+    return df
 
 def clean_data(df):
-    pass
+    """Clean the dataframe and remove dulpicates
 
+    Args:
+        df (pandas.DataFrame): [description]
+    """
+    categories = df["categories"].str.split(pat=';', expand=True)   # create a dataframe of the 36 individual category columns
+    row = categories.head(1)    # select the first row of the categories dataframe
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.applymap(lambda x: x[:-2]).iloc[0, :].tolist()
+    
+    categories.columns = category_colnames  # rename the columns of `categories`
+    for column in categories:
+        categories[column] = categories[column].astype(str).str[-1] # set each value to be the last character of the string
+        categories[column] = categories[column].astype(int) # convert column from string to numeric
+    
+    df.drop("categories", axis=1, inplace=True) # drop the original categories column from `df`
+    df = pd.concat([df, categories], axis=1)    # concatenate the original dataframe with the new `categories` dataframe
+    df.drop_duplicates(inplace=True)   # drop duplicates
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    """Save the dataframe as a sqlite db
+
+    Args:
+        df (pandas.DataFrame): DataFrame to be saved
+        database_filename (String): Name of Database to be saved as
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('MessageCategories', engine, index=False)  
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,91 @@
+# import libraries
 import sys
-
+import re
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sqlalchemy import create_engine
 
 def load_data(database_filepath):
-    pass
+    """Load date from the sqlite database
+
+    Args:
+        database_filepath (String): Database filename
+
+    Returns:
+        X , Y, category_names
+    """
+    # load data from database
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table("MessageCategories", con=engine)
+    X = df["message"]
+    y = df[df.columns[4:]]
+    category_names = y.columns
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    """Function to tokenize test messages
+
+    Args:
+        text (String): String to be tokenized
+
+    Returns:
+        clean_tokens (List): tokenized text as a list Object
+    """
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """Create pipeline and run a gridsearch based on parameters
 
+    Returns:
+        model: Returns the model type obtained by gridsearch
+    """
+    moc = MultiOutputClassifier(RandomForestClassifier(n_estimators=100))
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', moc)
+        ])
+    parameters = {'clf__estimator__max_depth': [10, 50, None],
+              'clf__estimator__min_samples_leaf':[2, 5, 10]}
+
+    cv = GridSearchCV(pipeline, parameters, cv=3)
+    return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
 
+    for i, col in enumerate(Y_test):
+        print(col)
+        print(classification_report(Y_test[col], y_pred[:, i]))
+    # print accuracy score
+    print('Accuracy: {}'.format(np.mean(Y_test.values == y_pred)))
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
